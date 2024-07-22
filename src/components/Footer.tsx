4276373b2a0dca59ebc5c8ae815fb5e20b62c66e
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-07-22 18:57:35 +0530

    ui: header and footer additions and subtractions

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,90 @@
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaXTwitter,
+  FaYoutube,
+} from "react-icons/fa6";
+import Logo from "./Logo";
+
 const Footer = () => {
-  return <footer className="px-32">Footer</footer>;
+  return (
+    <footer className="my-8 px-32">
+      <table className="mb-12 w-full table-fixed border-separate border-spacing-4">
+        <thead className="text-left text-lg">
+          <tr>
+            <th>
+              <Logo />
+            </th>
+            <th>About Us</th>
+            <th>Terms</th>
+            <th>Help Center</th>
+          </tr>
+        </thead>
+        <tbody className="text-sm *:my-8">
+          <tr>
+            <td></td>
+            <td>Contact</td>
+            <td>Privacy</td>
+            <td>Account</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>Support</td>
+            <td>Shipping</td>
+            <td>Wishlist</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>FAQ</td>
+            <td>Returns</td>
+            <td>Order History</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>Blog</td>
+            <td>Track Order</td>
+            <td>Featured Products</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>Careers</td>
+            <td>Customer Service</td>
+            <td>New Arrivals</td>
+          </tr>
+        </tbody>
+      </table>
+      <div className="mb-6 h-px bg-neutral-900"></div>
+      <div className="flex justify-between">
+        <div className="flex gap-x-12 *:text-xs">
+          <small>Copyright © 2024 Bazaar. All rights reserved.</small>
+          <ul className="flex items-center gap-x-8">
+            <li>Privacy Policy</li>
+            <li>Terms of Service</li>
+            <li>Cookie Settings</li>
+          </ul>
+        </div>
+        <div>
+          <ul className="flex gap-x-4">
+            <li>
+              <FaFacebook />
+            </li>
+            <li>
+              <FaInstagram />
+            </li>
+            <li>
+              <FaXTwitter />
+            </li>
+            <li>
+              <FaLinkedin />
+            </li>
+            <li>
+              <FaYoutube />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
 };
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,47 @@
+import Link from "next/link";
 import { Button } from "./Button";
 import Logo from "./Logo";
+import { useContext } from "react";
+import { AuthContext } from "@/app/(root)/layout";
+import { FaUser } from "react-icons/fa6";
 
 const Header = () => {
+  const auth = useContext(AuthContext);
+
   return (
     <header className="flex justify-between border-b border-b-primary bg-white px-32 py-4">
-      <ul className="flex items-center gap-6">
+      <ul className="flex items-center gap-x-12">
         <li>
-          <Logo />
+          <Link href="/">
+            <Logo />
+          </Link>
         </li>
-        <li>Shop now</li>
+        <li>
+          <Link href="products">Shop now</Link>
+        </li>
         <li>About Us</li>
         <li>Contact</li>
         <li>More</li>
       </ul>
       <div className="flex gap-4">
-        <Button intent="outline" size="sm">
-          Login
-        </Button>
-        <Button size="sm">Sign Up</Button>
+        {auth.isAuthenticated ? (
+          <Link href="/profile">
+            <Button className="rounded-full p-2.5">
+              <FaUser />
+            </Button>
+          </Link>
+        ) : (
+          <>
+            <Link href="/login">
+              <Button intent="outline" size="sm">
+                Login
+              </Button>
+            </Link>
+            <Link href="/signup">
+              <Button size="sm">Sign Up</Button>
+            </Link>
+          </>
+        )}
       </div>
     </header>
   );
