0d25fd4d466f11b8fb5854e7e790f05a3869a913
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-08-19 21:13:07 +0530

    ui: modify components header and toast

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,28 @@
 import Link from "next/link";
 import { Button } from "./Button";
 import Logo from "./Logo";
-import { useContext } from "react";
-import { AuthContext } from "@/app/(root)/layout";
 import { FaUser } from "react-icons/fa6";
+import { useSelector } from "react-redux";
+import { RootState } from "@/state/store";
+import { profile_popup_items } from "@/data";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { useRouter } from "next/navigation";
+import SearchBar from "./SearchBar";
+import { Search } from "./Search";
 
 const Header = () => {
-  const auth = useContext(AuthContext);
+  const Router = useRouter();
+  const auth = useSelector((state: RootState) => state.auth);
 
   return (
-    <header className="flex justify-between border-b border-b-primary bg-white px-32 py-4">
+    <header className="sticky top-0 z-10 flex items-center justify-between border-b border-b-primary bg-white/70 px-32 py-4 backdrop-blur">
       <ul className="flex items-center gap-x-12">
         <li>
           <Link href="/">
@@ -17,19 +30,41 @@ const Header = () => {
           </Link>
         </li>
         <li>
-          <Link href="products">Shop now</Link>
+          <Link href="/products">Shop now</Link>
         </li>
         <li>About Us</li>
-        <li>Contact</li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
         <li>More</li>
       </ul>
-      <div className="flex gap-4">
+      <div className="flex items-center gap-4">
+        <Search />
+        {/* <SearchBar /> */}
         {auth.isAuthenticated ? (
-          <Link href="/profile">
-            <Button className="rounded-full p-2.5">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="flex size-8 items-center justify-center rounded-full bg-primary p-2.5 text-white outline-none hover:bg-primary/90">
+              {/* <span className="rounded-full bg-primary p-2.5 outline-none"> */}
+              {/* <Button > */}
               <FaUser />
-            </Button>
-          </Link>
+              {/* </Button> */}
+              {/* </span> */}
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              {profile_popup_items.map((item) => (
+                <DropdownMenuItem
+                  key={item.label}
+                  className="cursor-pointer text-primary"
+                  onClick={() => item.onClick(Router)}
+                >
+                  <item.icon className="mr-2 h-4 w-4" />
+                  <span>{item.label}</span>
+                </DropdownMenuItem>
+              ))}
+            </DropdownMenuContent>
+          </DropdownMenu>
         ) : (
           <>
             <Link href="/login">
