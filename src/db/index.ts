271dd9f3d35f7a2a3fff0c19338c9c02a46b0fd6
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-09-07 17:25:20 +0530

    db: host database in supabase

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,11 @@
 import pg from "pg";
 
 const db = new pg.Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "bazaar",
-  password: "ravi4177",
-  port: 5432,
+  user: process.env.POSTGRES_USER,
+  host: process.env.POSTGRES_HOST,
+  database: process.env.POSTGRES_DATABASE,
+  password: process.env.POSTGRES_PASSWORD,
+  port: parseInt(process.env.POSTGRES_PORT!),
 });
 
 export default db;
