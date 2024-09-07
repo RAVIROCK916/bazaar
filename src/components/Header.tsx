84acb6ad4f67bce1f4fe285ad49063102b433834
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-09-07 19:08:11 +0530

    ignore lint during builds

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,6 +27,7 @@ import { FiArrowUpRight } from "react-icons/fi";
 import { CiSearch } from "react-icons/ci";
 import "@/styles/search.css";
 import { HiOutlineClock, HiOutlineTrash } from "react-icons/hi";
+import { Suspense } from "react";
 
 const Header = () => {
   const Router = useRouter();
@@ -123,110 +124,112 @@ const Header = () => {
   );
 
   return (
-    <header className="sticky top-0 z-10 flex items-center justify-between border-b border-b-primary bg-white/70 px-32 py-4 backdrop-blur">
-      <ul className="flex items-center gap-x-12">
-        <li>
-          <Link href="/">
-            <Logo />
-          </Link>
-        </li>
-        <li>
-          <Link href="/products" className="uppercase">
-            Shop now
-          </Link>
-        </li>
-        <li>
-          <Link href="/about" className="uppercase">
-            About Us
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact" className="uppercase">
-            Contact
-          </Link>
-        </li>
-        <li className="uppercase">More</li>
-      </ul>
-      <div className="flex items-center gap-4">
-        <Autocomplete
-          initialState={{
-            query: (searchParams.get("q") as string) || "",
-          }}
-          openOnFocus={true}
-          placeholder="Search for products"
-          detachedMediaQuery="(max-width: 1024px)"
-          classNames={{
-            form: "relative rounded-md flex-1 border border-neutral-700",
-            inputWrapperPrefix:
-              "absolute inset-y-0 left-0 flex items-center pl-3",
-            inputWrapperSuffix:
-              "absolute inset-y-0 right-0 flex items-center pr-2",
-            label: "flex items-center",
-            submitButton: "h-5 w-5 text-gray-400",
-            clearButton: "h-5 w-5 text-gray-400",
-            input:
-              "block w-full rounded-md outline-none border-neutral-300 py-2.5 pl-10 text-sm",
-            panel:
-              "flex-1 lg:flex-none lg:absolute z-10  lg:text-sm text-gray-500 bg-white lg:shadow-lg lg:rounded-b-md overflow-y-scroll lg:max-h-96",
-            detachedSearchButton: "p-2 text-gray-400 hover:text-gray-500",
-            detachedSearchButtonPlaceholder: "sr-only",
-            detachedSearchButtonIcon:
-              "w-6 h-6 flex items-center justify-center",
-            detachedContainer:
-              "fixed inset-0 flex flex-col divide-y divide-gray-200/50",
-            detachedFormContainer: "flex p-2 bg-white",
-            detachedCancelButton:
-              "bg-white px-2 ml-2 text-gray-500 hover:text-gray-600 transition-colors",
-          }}
-          className="w-full min-w-96 max-w-4xl"
-          navigator={{
-            navigate({ itemUrl }) {
-              Router.push(itemUrl);
-            },
-          }}
-          onSubmit={({ state }) => {
-            Router.push(`/products/search/?q=${state.query}`);
-          }}
-          plugins={[getRecentSearchesPlugin(), getQuerySuggestionsPlugin()]}
-        />
-        {auth.isAuthenticated ? (
-          <DropdownMenu>
-            <DropdownMenuTrigger className="flex size-8 items-center justify-center rounded-full bg-primary p-2.5 text-white outline-none hover:bg-primary/90">
-              {/* <span className="rounded-full bg-primary p-2.5 outline-none"> */}
-              {/* <Button > */}
-              <FaUser />
-              {/* </Button> */}
-              {/* </span> */}
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              {profile_popup_items.map((item) => (
-                <DropdownMenuItem
-                  key={item.label}
-                  className="cursor-pointer text-primary"
-                  onClick={() => item.onClick(Router)}
-                >
-                  <item.icon className="mr-2 h-4 w-4" />
-                  <span>{item.label}</span>
-                </DropdownMenuItem>
-              ))}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        ) : (
-          <>
-            <Link href="/login">
-              <Button intent="outline" size="sm">
-                Login
-              </Button>
+    <Suspense fallback={<div>Loading...</div>}>
+      <header className="sticky top-0 z-10 flex items-center justify-between border-b border-b-primary bg-white/70 px-32 py-4 backdrop-blur">
+        <ul className="flex items-center gap-x-12">
+          <li>
+            <Link href="/">
+              <Logo />
             </Link>
-            <Link href="/signup">
-              <Button size="sm">Sign Up</Button>
+          </li>
+          <li>
+            <Link href="/products" className="uppercase">
+              Shop now
             </Link>
-          </>
-        )}
-      </div>
-    </header>
+          </li>
+          <li>
+            <Link href="/about" className="uppercase">
+              About Us
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" className="uppercase">
+              Contact
+            </Link>
+          </li>
+          <li className="uppercase">More</li>
+        </ul>
+        <div className="flex items-center gap-4">
+          <Autocomplete
+            initialState={{
+              query: (searchParams.get("q") as string) || "",
+            }}
+            openOnFocus={true}
+            placeholder="Search for products"
+            detachedMediaQuery="(max-width: 1024px)"
+            classNames={{
+              form: "relative rounded-md flex-1 border border-neutral-700",
+              inputWrapperPrefix:
+                "absolute inset-y-0 left-0 flex items-center pl-3",
+              inputWrapperSuffix:
+                "absolute inset-y-0 right-0 flex items-center pr-2",
+              label: "flex items-center",
+              submitButton: "h-5 w-5 text-gray-400",
+              clearButton: "h-5 w-5 text-gray-400",
+              input:
+                "block w-full rounded-md outline-none border-neutral-300 py-2.5 pl-10 text-sm",
+              panel:
+                "flex-1 lg:flex-none lg:absolute z-10  lg:text-sm text-gray-500 bg-white lg:shadow-lg lg:rounded-b-md overflow-y-scroll lg:max-h-96",
+              detachedSearchButton: "p-2 text-gray-400 hover:text-gray-500",
+              detachedSearchButtonPlaceholder: "sr-only",
+              detachedSearchButtonIcon:
+                "w-6 h-6 flex items-center justify-center",
+              detachedContainer:
+                "fixed inset-0 flex flex-col divide-y divide-gray-200/50",
+              detachedFormContainer: "flex p-2 bg-white",
+              detachedCancelButton:
+                "bg-white px-2 ml-2 text-gray-500 hover:text-gray-600 transition-colors",
+            }}
+            className="w-full min-w-96 max-w-4xl"
+            navigator={{
+              navigate({ itemUrl }) {
+                Router.push(itemUrl);
+              },
+            }}
+            onSubmit={({ state }) => {
+              Router.push(`/products/search/?q=${state.query}`);
+            }}
+            plugins={[getRecentSearchesPlugin(), getQuerySuggestionsPlugin()]}
+          />
+          {auth.isAuthenticated ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger className="flex size-8 items-center justify-center rounded-full bg-primary p-2.5 text-white outline-none hover:bg-primary/90">
+                {/* <span className="rounded-full bg-primary p-2.5 outline-none"> */}
+                {/* <Button > */}
+                <FaUser />
+                {/* </Button> */}
+                {/* </span> */}
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                {profile_popup_items.map((item) => (
+                  <DropdownMenuItem
+                    key={item.label}
+                    className="cursor-pointer text-primary"
+                    onClick={() => item.onClick(Router)}
+                  >
+                    <item.icon className="mr-2 h-4 w-4" />
+                    <span>{item.label}</span>
+                  </DropdownMenuItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <>
+              <Link href="/login">
+                <Button intent="outline" size="sm">
+                  Login
+                </Button>
+              </Link>
+              <Link href="/signup">
+                <Button size="sm">Sign Up</Button>
+              </Link>
+            </>
+          )}
+        </div>
+      </header>
+    </Suspense>
   );
 };
 export default Header;
