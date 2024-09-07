837530a1363020724d7c3facc660bad76c69bc8e
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-09-07 18:05:16 +0530

    build: changes for deployment

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,4 +8,12 @@ const db = new pg.Pool({
   port: parseInt(process.env.POSTGRES_PORT!),
 });
 
+db.connect()
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+  });
+
 export default db;
