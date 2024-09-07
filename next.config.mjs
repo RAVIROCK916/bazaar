17899a0b03483482fbb6c3df986b244891d2d291
Ravi Teja Pedapudi <prt15@iitbbs.ac.in>
2024-09-07 19:11:19 +0530

    ignore lint during builds

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,9 @@ const nextConfig = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
